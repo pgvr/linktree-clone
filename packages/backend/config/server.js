3821e86acbe5861eaae73044d6acc7523e457d49
patrickgoeler <patrickvongoeler@gmail.com>
2021-01-21 09:31:08 +0100

    push backend

---
+++ packages/backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd117dffe4a81c81ddd45f11b7898eb41'),
+    },
+  },
+});
---
+++ packages/backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c6896f76-8e3c-44b1-a1ba-b8fb2c5a4c12'
+};
